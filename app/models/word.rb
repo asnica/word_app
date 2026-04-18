66039835d3c275750c5bf7f226082ead4abd75e1
hein11452-afk <hein11452@gmail.com>
2026-04-18 18:56:48 +0900

    単語・タグ管理機能の強化およびUIのブラッシュアップ

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,7 @@
 class Word < ApplicationRecord
   belongs_to :user
-  belongs_to :tag
+  has_many :word_tags, dependent: :destroy
+  has_many :tags, through: :word_tags
 
   # バリデーションの設定
   validates :name, presence: true
